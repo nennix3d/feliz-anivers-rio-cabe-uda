8378ecfbedc857d50132b5a34473d6dffd9ec144
nennix3d <nicolasdossantosdemoraes@gmail.com>
2026-01-30 12:35:18 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const botao = document.getElementById("surpresaBtn");
+const mensagem = document.getElementById("mensagem");
+const musica = document.getElementById("musica");
+const fotos = document.querySelectorAll(".foto");
+
+botao.addEventListener("click", () => {
+    mensagem.classList.remove("escondido");
+
+    fotos.forEach(foto => {
+        foto.classList.remove("escondido");
+    });
+
+    musica.play();
+
+    for (let i = 0; i < 80; i++) {
+        criarConfete();
+    }
+
+    botao.style.display = "none";
+});
+
+function criarConfete() {
+    const confete = document.createElement("div");
+    confete.classList.add("confete");
+
+    confete.style.left = Math.random() * window.innerWidth + "px";
+    confete.style.backgroundColor =
+        `hsl(${Math.random() * 360}, 100%, 50%)`;
+
+    document.body.appendChild(confete);
+
+    setTimeout(() => confete.remove(), 3000);
+}
 
